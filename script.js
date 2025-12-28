dad4f2ec37604de34f5ad0212eee50f393f5d5d2
dncworking <dominyka.dobuzinskaite@gmail.com>
2025-12-28 17:17:52 +0200

    work done

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 "use strict";
+
+const counterNum = document.querySelector("#counter");
+const button = document.querySelector("#counter-btn");
+let count = 0;
+
+button.addEventListener("click", function () {
+  count++;
+  counterNum.textContent = count;
+});
+//======================================================
+
+const liveInput = document.querySelector("#live-input");
+const preview = document.querySelector("#preview");
+
+liveInput.addEventListener("input", function () {
+  preview.textContent = liveInput.value;
+});
+//===========================================================
+const hoverBox = document.querySelector("#hover-box");
+
+hoverBox.addEventListener("mouseenter", function () {
+  hoverBox.style.backgroundColor = "yellow";
+});
+
+hoverBox.addEventListener("mouseleave", function () {
+  hoverBox.style.backgroundColor = "";
+});
